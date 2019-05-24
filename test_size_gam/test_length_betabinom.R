104b6e96a0c65832bf3a506025f1bb68664e954d
Yin <YINYI@dfo-mpo.ca>
2019-05-24 09:20:37 -0300

    read NED2016 data and fit length based beta binomial model

---
+++ test_size_gam/test_length_betabinom.R
@@ -0,0 +1,95 @@
+# #########################################
+# test beta binomial model
+# #########################################
+
+rm(list = ls())
+setwd("C:/Users/yinyi/Dropbox/BIO/Comparative_Fishing/Workspace/test_size_gam/")
+
+load("data-NED2016.RData")
+
+# -----------------------------------------
+# data for model 
+
+d.length %>% count(species, name) %>% print(n=Inf)
+
+i.species <- 11
+
+d <- d.length %>% 
+    filter(species == i.species) %>%
+    transmute(station, gear, len, c) %>%
+    spread(gear, c, fill = 0) %>%
+    # mutate(len = (floor(len/10))) %>% # length grouping
+    group_by(len) %>% 
+    summarise(A = (sum(`9`)), B = (sum(`15`)))%>%
+    mutate(N = A+B) %>%
+    complete(len = full_seq(len, 1), fill = list(A=0,B=0,N=0)) %>%
+    mutate(A = A/min(N[N!=0])*2,
+           B = B/min(N[N!=0])*2,
+           N = N/min(N[N!=0])*2) %>% # for gamlss computation, blow up min number to 2
+    print(n=Inf)
+
+
+# -----------------------------------------
+# fit model
+# overdispersion (of a beta distribution) only exists when trials > 1
+# higher degree of freedom may not be satified given "bad" data
+
+library(gamlss)
+rm("fit")
+fit <- gamlss(
+    formula = cbind(A, B) ~ cs(len),
+    sigma.formula = ~ len,
+    family = BB(mu.link = "logit", sigma.link = "log"),
+    data = d)
+
+summary(fit)
+
+
+# -----------------------------------------
+# results plot
+
+jpeg(filename = paste("beta-binom/fitted species", i.species, ".jpg"), units = "in", res = 200, width = 4, height = 6)
+fittedPlot(fit, x = d$len)
+dev.off()
+
+
+# Proportion of A
+jpeg(filename = paste("beta-binom/fit with CI95(fast simu) species", i.species, ".jpg"), units = "in", res = 200, width = 6, height = 5)
+plot(d$len, d$A/d$N, ylim = c(0, 1), ylab = "Prop. of Gear 9")
+lines(d$len, fit$mu.fv)
+# computation of CI is based on simulation of estimated BB dist
+# median more robust than mean
+lines(d$len, qBB(0.5, mu = fit$mu.fv, sigma = fit$sigma.fv, bd = 1000)/1000, col = "blue")
+lines(d$len, qBB(0.05, mu = fit$mu.fv, sigma = fit$sigma.fv, bd = 1000)/1000, col = "blue", lty = "dashed")
+lines(d$len, qBB(0.95, mu = fit$mu.fv, sigma = fit$sigma.fv, bd = 1000)/1000, col = "blue", lty = "dashed")
+dev.off()
+
+# conversion factor: rho
+jpeg(filename = paste("beta-binom/rho with CI95(fast simu) species", i.species, ".jpg"), units = "in", res = 200, width = 6, height = 5)
+plot(d$len, d$A/d$B, ylab = "rho: Gear 9 / Gear 15")
+lines(d$len, fit$mu.fv/(1-fit$mu.fv))
+abline(a = 1, b = 0)
+# CI approximated by lognormal given variance of logit(P)
+lines(d$len, fit$mu.fv/(1-fit$mu.fv)-1.96*sqrt(fit$mu.var) * (fit$mu.fv/(1-fit$mu.fv)), col = "blue", lty = "dashed")
+lines(d$len, fit$mu.fv/(1-fit$mu.fv)+1.96*sqrt(fit$mu.var) * (fit$mu.fv/(1-fit$mu.fv)), col = "blue", lty = "dashed")
+dev.off()
+
+# -----------------------------------------
+# spatial exploratory
+# map setting
+base_map <- ggplot() +
+    borders("world", colour="gray50", fill = "gray90", xlim = c(-67,-65), ylim = c(43,45)) +
+    coord_map(xlim = c(-63,-57), ylim = c(43, 46)) +
+    theme_bw() +
+    theme(axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(), 
+          legend.position = "bottom")
+
+base_map +
+    geom_point(data = d.length %>%
+                   filter(species == i.species) %>% 
+                   mutate(len = as.factor(floor(len/10))),
+               aes(lon, lat, color = gear, size = c), shape = 21) +
+    facet_wrap(~len)
+
---
+++ test_size_gam/test_read_data.R
@@ -0,0 +1,163 @@
+# #######################################
+# Read data: NED2016
+# #######################################
+
+rm(list = ls())
+setwd("C:/Users/yinyi/Dropbox/BIO/Comparative_Fishing/Workspace/test_size_gam/")
+
+library(dplyr)
+library(tidyr)
+
+# read tables
+library("xlsx")
+data.path <- "../../Data/20190510-Don-Survey/NED2016116.xlsx"
+data.set <- read.xlsx(data.path, sheetIndex = 1, header = T) 
+data.catch <- read.xlsx(data.path, sheetIndex = 2, header = T) 
+data.length <- read.xlsx(data.path, sheetIndex = 3, header = T, endRow = 3163)
+
+# map settings
+library(maps)
+library(ggplot2)
+base_map <- ggplot() +
+    borders("world", colour="gray50", fill = "gray90", xlim = c(-67,-65), ylim = c(43,45)) +
+    coord_fixed(ratio = 1.2, xlim = c(-64,-56), ylim = c(43, 46)) +
+    theme_bw() +
+    theme(
+        axis.title = element_blank(),
+        legend.position = "bottom"
+    )
+
+# #########################################
+# set info
+# #########################################
+
+# check data
+
+ggplot(data.set) +
+    geom_histogram(aes(x = DIST)) +
+    facet_wrap(~as.factor(GEAR), ncol = 1)
+
+ggplot() +
+    geom_point(data = data.set, aes(as.factor(STATION),DUR, color = as.factor(GEAR))) +
+    geom_text(data = data.set %>% filter(TYPE!=5), aes(as.factor(STATION),DUR+1, label = TYPE, color = as.factor(GEAR))) 
+
+# organize
+
+d.set <- data.set %>%
+    filter(
+        TYPE == 5,# successful compartative tows
+        !is.na(DIST)# valid distance data
+    )%>% 
+    transmute(
+        mission = MISSION,
+        gear = as.factor(GEAR),
+        stratum = as.integer(STR), 
+        station = as.integer(STATION),
+        setno = as.integer(SETNO),
+        dmin = DMIN,
+        duration = DUR,
+        distance = DIST, 
+        lon = - (floor(SLONG/100) + SLONG%%100/60), # convert lat-long
+        lat = floor(SLAT/100) + SLAT%%100/60
+    ) %>%
+    filter(
+        station!=55 # remove one tow: short distance/duration
+    )
+
+
+# summarize
+
+d.set %>%
+    group_by(gear) %>%
+    summarise(
+        m_dist = mean(distance), 
+        n_station = n()
+    )
+    
+
+# visualize
+
+base_map +
+    geom_point(data = d.set, aes(lon, lat, shape = gear, color = gear)) +
+    geom_text(data = d.set %>% group_by(station) %>% slice(1), aes(lon, lat+0.1, label = station), size = 2)
+ggsave("tow_locations_map.pdf", width = 8, height = 5)
+
+
+# #########################################
+# catch
+# #########################################
+
+
+# check data
+
+data.catch %>% 
+    count(spec, comm) %>%
+    ggplot() +
+    geom_histogram(aes(n))
+
+
+# organize
+
+d.catch <- data.catch %>%
+    transmute(
+        mission,
+        gear = as.factor(gear),
+        stratum = as.integer(strat), 
+        station = as.integer(station),
+        species = as.factor(spec),
+        name = comm,
+        n = totno,
+        w = totwgt
+    )
+
+
+# visualize: catch by tow, by species, by gear
+
+d.catch %>%
+    ggplot() +
+    geom_point(aes(as.factor(station), n, color = gear, shape = gear)) +
+    facet_wrap(~paste(species, name), scales = "free", ncol = 4) +
+    theme_bw() + theme(legend.position = "top")
+ggsave(filename = "paired_catch_n_per_species.pdf", dpi = 300, width = 10, height = 40)
+
+
+
+# #########################################
+# Join tables with length frequency
+# #########################################
+
+
+d.length <- data.length %>% 
+    transmute(
+        mission,
+        station = as.integer(station),
+        setno = as.integer(setno),
+        species = as.integer(spec),
+        name = as.factor(comm),
+        flen, clen
+        ) %>%
+    inner_join(d.set, by = c("mission", "station", "setno")) %>%
+    mutate(
+        len = flen,
+        c = clen/distance # adjust catch for tow distance
+    )
+
+
+# visualize: length frenquency by tow, by species, by gear
+
+ggplot(d.length) +
+    geom_line(aes(len, c, group = station), color = "gray") +
+    stat_summary(aes(len, c), fun.y=mean, geom="line", colour="red", alpha = 0.3, size = 1.2) +
+    facet_grid(paste(species, substr(name,1,12))~gear, scales = "free_y", switch = "y") +
+    theme_bw()
+ggsave(filename = "catch_length_per_species.pdf", dpi = 300, width = 8, height = 80, limitsize = F)
+
+
+
+# #########################################
+# Save data frame
+# #########################################
+
+save(file = "data-NED2016.RData", d.length)
+
+
